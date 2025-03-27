c0749a8db5afe54ac844b1d2f3ce4f686dfa0d26
Joshua Munchies <159332348+linuxmunchies@users.noreply.github.com>
2025-03-27 12:07:04 -0500

    Update archfox.sh
    Remove invalid comment

--- archfox.sh
+++ archfox.sh
@@ -338,7 +338,7 @@ install_essentials() {
   # Install packages
   install_package "amdgpu_top intel-gpu-tools bluez-utils duf fastfetch flatpak btop htop rsync inxi fzf ncdu tmux git wget curl kitty bat make unzip unrar vim wl-clipboard gcc go tldr zsh"
 
-  # Install Resources
+  # Install Essential Flatpaks
   flatpak install net.nokyan.Resources im.riot.Riot io.github.dimtpap.coppwr org.nickvision.cavalier org.telegram.desktop com.rustdesk.RustDesk com.github.unrud.VideoDownloader com.github.tchx84.Flatseal org.kde.kwalletmanager5 -y
   # Install rclone
   log_message "Installing rclone..."
